19474ebba48e80c7d9b5b4f4505a0982c8d16d24
as1216 <arshiya.singh16@gmail.com>
2015-09-26 18:03:27 -0400

    add files

--- location.js
+++ location.js
@@ -1,17 +1,63 @@
-function initAutocomplete() {
-  var autocomplete = new google.maps.places.Autocomplete(document
-    .getElementById('autocomplete'));
-  google.maps.event.addListener(autocomplete, 'locationField', geolocate());
-}
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -33.8688, lng: 151.2195},
+    zoom: 13
+  });
+  var input = (
+      document.getElementById('pac-input'));
 
-  
-function geolocate() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var geolocation = new google.maps.LatLng(position.coords
-        .latitude,position.coords.longitude);
-      autocomplete.setBounds(new google.maps
-        .LatLngBounds(geolocation, geolocation));
-    });
-  }
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', map);
+
+  var infowindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: map,
+    anchorPoint: new google.maps.Point(0, -29)
+  });
+  var circle = new google.maps.Circle({
+    map: map,
+    radius: 16093,    // 10 miles in metres
+    fillColor: '#2EFEF7'
+  });
+  circle.bindTo('center', marker, 'position');
+
+  autocomplete.addListener('place_changed', function() {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      window.alert("This place isn't found");
+      return;
+    }
+
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(13);
+    }
+    marker.setIcon(({
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(35, 35)
+    }));
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(map, marker);
+  });
 }
